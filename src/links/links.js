1bc483a249d74f971f558de796d674b10c75e6a7
Adrian Marquis <adrian.marquis78@gmail.com>
2020-10-30 07:16:46 -0400

    anchor tags updated in Links

--- src/links/links.js
+++ src/links/links.js
@@ -5,13 +5,13 @@ export default function Links() {
     return (
         <div>
             <h3>Try Something Different:</h3>
-            <a href to='https://www.meetup.com/topics/outdoor-yoga/us' target='_blank'>Outdoor Yoga</a>
-            <a href to='https://www.meetup.com/topics/cycling/us/' target='_blank'>Cycling Groups</a>
-            <a href to='https://www.meetup.com/topics/running/us/' target='_blank'>Running Groups</a>
-            <a href to='https://www.meetup.com/topics/fitness/us/' target='_blank'>Fitness Meetups</a>
-            <a href to='https://www.alltrails.com/' target='_blank'>Hiking Trails</a>
-            <a href to='https://www.verywellfit.com/best-online-exercise-classes-4163381' target='_blank'>Online Fitness</a>
-            <a href to='https://www.anytimefitness.com/find-gym/' target='_blank'>Local Gyms</a>
+            <a href='https://www.meetup.com/topics/outdoor-yoga/us' target='_blank'>Outdoor Yoga</a>
+            <a href='https://www.meetup.com/topics/cycling/us/' target='_blank'>Cycling Groups</a>
+            <a href='https://www.meetup.com/topics/running/us/' target='_blank'>Running Groups</a>
+            <a href='https://www.meetup.com/topics/fitness/us/' target='_blank'>Fitness Meetups</a>
+            <a href='https://www.alltrails.com/' target='_blank'>Hiking Trails</a>
+            <a href='https://www.verywellfit.com/best-online-exercise-classes-4163381' target='_blank'>Online Fitness</a>
+            <a href='https://www.anytimefitness.com/find-gym/' target='_blank'>Local Gyms</a>
         </div>
     )
 }
